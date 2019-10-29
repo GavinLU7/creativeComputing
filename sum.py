0c8863a5b249b9a47e9296190bb55fca424746ed
GavinLU7 <luxiyan@outlook.com>
2019-10-29 17:54:57 +0900

    fixed symbol on second position

--- sum.py
+++ sum.py
@@ -3,32 +3,33 @@ num = 0
 count = 0
 symbol = ''
 while num != '':
-	num = float(num)
-	sum1 += num
-	count += 1
-	if count // 10 == 1 or count % 10 > 3:
-		symbol = 'th'
-	elif count % 10 == 0:
-		symbol = 'th'
-	elif count % 10 == 1:
-		symbol = 'st'
-	elif count % 10 == 2:
-		symbol = 'nd'
-	else:
-		symbol == 'rd'
-	num = input('Please input the '+ str(count) + symbol + ' number: ')
-	isnum = False
-	while isnum != True and num != '':
-		preventError = num
-		preventError = list(preventError)
-		if num.find('.') != -1:
-			preventError.remove('.')
-		if num.find('-') != -1:
-			preventError.remove('-')
-		preventError = ''.join(preventError)
-		isnum = preventError.isnumeric()
-		if isnum != True:
-			num = input('Please input correct the number: ')         
+        num = float(num)
+        sum1 += num
+        count += 1
+        if count // 10 == 1 or count % 10 > 3:
+                symbol = 'th'
+        elif count % 10 == 0:
+                symbol = 'th'
+        elif count % 10 == 1:
+                symbol = 'st'
+        elif count % 10 == 2:
+                symbol = 'nd'
+        else:
+                symbol == 'rd'
+        num = input('Please input the '+ str(count) + symbol + ' number: ')
+        isnum = False
+        while not isnum  and num != '':
+                preventError = num
+                preventError = list(preventError)
+                if num.find('-') == 0 :
+                        preventError.remove('-')
+                if num.find('.') > 0 :
+                        preventError.remove('.')
+                preventError = ''.join(preventError)
+                isnum = preventError.isnumeric()
+		
+                if not isnum:
+                        num = input('Please input correct the number: ')         
 		
 print('Your input', count-1, 'numbers')	
 print('sum of numbers:',sum1)
